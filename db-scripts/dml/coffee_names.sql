ae382d685f55bf6b2982ec8c9f2f074b0a29360d
BalakrishnaPro <bala.csit@gmail.com>
2019-07-29 22:34:05 +0530

    Added scripts to inserts coffee names and coffee types.

---
+++ db-scripts/dml/coffee_names.sql
@@ -0,0 +1,5 @@
+INSERT INTO tbl_coffee_name(coffee_name) VALUES('Cafe Coffee Day');
+INSERT INTO tbl_coffee_name(coffee_name) VALUES('Barista');
+INSERT INTO tbl_coffee_name(coffee_name) VALUES('Beyond Coffee');
+INSERT INTO tbl_coffee_name(coffee_name) VALUES('Starbucks Coffee');
+COMMIT;
---
+++ db-scripts/dml/coffee_types.sql
@@ -0,0 +1,7 @@
+INSERT INTO tbl_coffee_type(coffee_type) VALUES('Short Macchiato');
+INSERT INTO tbl_coffee_type(coffee_type) VALUES('Long Macchiato');
+INSERT INTO tbl_coffee_type(coffee_type) VALUES('Ristretto');
+INSERT INTO tbl_coffee_type(coffee_type) VALUES('Cappuccino');
+INSERT INTO tbl_coffee_type(coffee_type) VALUES('Mocha');
+INSERT INTO tbl_coffee_type(coffee_type) VALUES('Affogato');
+COMMIT;
